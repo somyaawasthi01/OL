c90e149c81885bfab3a7f80d513fadd8d3eeef7e
SOMYA AWASTHI <somyaawasthi2001@gmail.com>
2026-01-18 00:58:37 +0530

    Update import path for IdeaList component

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { IdeaList } from './components/temp';
+import { IdeaList } from './components/Idealist.tsx';
 import { LoveLetter } from './components/LoveLetter';
 import { QuickTexts } from './components/QuickTexts';
 import { Counselling } from './components/Counselling';
